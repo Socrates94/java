c246c3aae5089bd70111cee1d11a659a733ce697
Daniel Flores <daniel_f94@outlook.com>
2024-03-26 14:47:00 -0600

    Creando nuevo directorio y moviendo archivo app

---
+++ Algoritmos/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i < 10; i++){
+            System.out.println("Homla soy cheems y estoy practicando git con github en windows.");
+        }
+
+    }
+}
